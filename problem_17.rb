f7cec9259698db9cd159e21073c60f593657e09f
Michael <Michael.ginalick@gmail.com>
2015-08-28 19:28:34 -0500

    added num hash

---
+++ problem_17.rb
@@ -0,0 +1,51 @@
+#If the numbers 1 to 5 are written out in words: 
+#one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+#If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, 
+#how many letters would be used?
+
+
+#NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) 
+#contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. 
+#The use of "and" when writing out numbers is in compliance with British usage.
+
+NUM_HASH = {
+
+	"one" => 3,
+	"two" => 3,
+	"three" => 5,
+	"four" => 4,
+	"five" => 4,
+	"six" => 3,
+	"seven" => 5,
+	"eight" => 5,
+	"nine" => 4,
+	"ten" => 3,
+	"eleven" => 6,
+	"twelve" => 6,
+	"thirteen" => 8,
+	"twenty" => 6,
+	"thirty" => 6,
+	"forty" => 5,
+	"fifty" => 5,
+	"sixty" => 5,
+	"seventy" => 7,
+	"eighty" => 6,
+	"ninety" => 6,
+	"hundred" => 7,
+	"thousand" => 8
+
+}
+
+
+def letters
+
+	1.upto(1000) do |i|
+		
+	end
+
+
+end
+
+
+
